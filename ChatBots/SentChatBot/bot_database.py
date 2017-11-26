75fc50ea0a1a297ae43760c81836f582df75547c
Heriberto Prieto <duskayame@gmail.com>
2017-11-25 18:17:46 -0800

    finished chatbot database

--- ChatBots/SentChatBot/bot_database.py
+++ ChatBots/SentChatBot/bot_database.py
@@ -3,4 +3,16 @@ import json
 from datetime import datetime
 
 timeframe = '2015-05'
+
+# for building inserting entire transactions of data at once
 sql_transaction = []
+
+connection = sqlite3.connect('{}.db'.format(timeframe))
+
+c = connection.cursor()
+
+def create_table():
+    # query
+    c.execute('CREATE TABLE IF NOT EXISTS parent_reply(parent_id TEXT PRIMARY KEY), comment_id TEXT UNIQUE, parent TEXT, comment TEXT, subreddit TEXT, unix INT, score INT')
+    if __name__=="__main__":
+        create_table()
